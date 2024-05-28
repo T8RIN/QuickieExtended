c4eb832ed03d12e783c6aa0013ad844d62b12f5a
T8RIN <oolbp@bk.ru>
2024-05-29 02:26:20 +0300

    Change to Zxing

--- quickie/build.gradle.kts
+++ quickie/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.parcelize)
   alias(libs.plugins.kotlin.dokka)
-  `maven-publish`
+  id("maven-publish")
 }
 
 afterEvaluate {
