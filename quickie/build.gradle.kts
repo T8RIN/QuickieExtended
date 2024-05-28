60e8caf077ccd2393721e3da815439d1fa7ee579
T8RIN <oolbp@bk.ru>
2024-05-29 02:21:21 +0300

    Change to Zxing

--- quickie/build.gradle.kts
+++ quickie/build.gradle.kts
@@ -4,7 +4,15 @@ plugins {
   alias(libs.plugins.kotlin.parcelize)
   alias(libs.plugins.kotlin.dokka)
   `maven-publish`
-  signing
+}
+
+afterEvaluate {
+  publishing {
+    publications {
+      create<MavenPublication>("bundledRelease") { commonConfig("bundled") }
+      create<MavenPublication>("unbundledRelease") { commonConfig("unbundled") }
+    }
+  }
 }
 
 android {
@@ -44,87 +52,10 @@ dependencies {
 group = "io.github.g00fy2.quickie"
 version = libs.versions.quickie.get()
 
-tasks.register<Jar>("androidJavadocJar") {
-  archiveClassifier = "javadoc"
-  from(layout.buildDirectory.dir("dokka/javadoc"))
-  dependsOn("dokkaJavadoc")
-}
-
-tasks.register<Jar>("androidBundledSourcesJar") {
-  archiveClassifier = "sources"
-  from(android.sourceSets.getByName("main").java.srcDirs, android.sourceSets.getByName("bundled").java.srcDirs)
-}
-
-tasks.register<Jar>("androidUnbundledSourcesJar") {
-  archiveClassifier = "sources"
-  from(android.sourceSets.getByName("main").java.srcDirs, android.sourceSets.getByName("unbundled").java.srcDirs)
-}
-
-afterEvaluate {
-  tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-    testLogging.events("failed", "passed", "skipped")
-    enabled = name.endsWith("DebugUnitTest")
-  }
-
-  publishing {
-    publications {
-      create<MavenPublication>("bundledRelease") { commonConfig("bundled") }
-      create<MavenPublication>("unbundledRelease") { commonConfig("unbundled") }
-    }
-    repositories {
-      maven {
-        name = "sonatype"
-        url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-        credentials {
-          username = findStringProperty("sonatypeUsername")
-          password = findStringProperty("sonatypePassword")
-        }
-      }
-    }
-  }
-}
-
-signing {
-  findStringProperty("signing.keyId")
-  findStringProperty("signing.password")
-  findStringProperty("signing.secretKeyRingFile")
-  sign(publishing.publications)
-}
 
 fun MavenPublication.commonConfig(flavor: String) {
   from(components["${flavor}Release"])
   artifactId = "quickie-$flavor"
-  artifact(tasks.named("androidJavadocJar"))
-  artifact(tasks.named("android${flavor.replaceFirstChar { it.titlecase() }}SourcesJar"))
-  pom {
-    name = "quickie-$flavor"
-    description = "Android QR code scanning library"
-    url = "https://github.com/G00fY2/quickie"
-    licenses {
-      license {
-        name = "MIT License"
-        url = "https://opensource.org/licenses/MIT"
-      }
-    }
-    developers {
-      developer {
-        id = "g00fy2"
-        name = "Thomas Wirth"
-        email = "twirth.development@gmail.com"
-      }
-    }
-    scm {
-      connection = "https://github.com/G00fY2/quickie.git"
-      developerConnection = "https://github.com/G00fY2/quickie.git"
-      url = "https://github.com/G00fY2/quickie"
-    }
-  }
-}
-
-fun Project.findStringProperty(propertyName: String): String? {
-  return findProperty(propertyName) as String? ?: run {
-    println("$propertyName missing in gradle.properties")
-    null
-  }
+  version = "1.10.0"
+  groupId = "com.github.t8rin"
 }
